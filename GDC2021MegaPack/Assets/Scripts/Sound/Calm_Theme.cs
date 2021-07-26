7938f987196c1d27871f7c7ca75b3347a50d2a48
GDC2021-G1-Lyd <teknisk@game.unf.dk>
2021-07-26 16:03:38 +0200

    Tilføjede overgang, men den er ikke færdig

--- GDC2021MegaPack/Assets/Scripts/Sound/Calm_Theme.cs
+++ GDC2021MegaPack/Assets/Scripts/Sound/Calm_Theme.cs
@@ -1,16 +1,22 @@
-using System.Collections;
+ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Calm_Theme : MonoBehaviour
 {
-    public AudioClip calm_1, calm_2, calm_3;
+    public AudioClip calm_1, calm_2, calm_3, overgang, didgerido, deep;
     public AudioSource audio;
+
+    public float originVolume;
 
     private int PrevNum = 0;
 
+    bool overgang_has_played = false;
+
+
     void Start() 
     {
+        audio.volume = originVolume;
         //Start by playing the pure marimba theme
         audio.Play();
     }
@@ -18,7 +24,7 @@ public class Calm_Theme : MonoBehaviour
     void FixedUpdate()
     {
         //Only play sound is audio is not currently playing
-        if (!audio.isPlaying)
+        if (!audio.isPlaying && ScoreHandler.playerScore < 185.0f)
         {
             switch (Get_Random_Number())
             {
@@ -36,6 +42,25 @@ public class Calm_Theme : MonoBehaviour
             audio.Play();
         }
 
+        //Play overgang
+        else if (!audio.isPlaying && !overgang_has_played)
+        {
+            audio.clip = overgang;
+            audio.Play();
+            overgang_has_played = true;
+        }
+
+        //Spil solo didgerido
+        else if (ScoreHandler.playerScore > 300 && ScoreHandler.playerScore < 400 && overgang_has_played) 
+        {
+             
+            audio.volume = 0 + (((ScoreHandler.playerScore - 300.0f) * (6.0f / 10.0f)) / 100.0f);
+            if (!audio.isPlaying) 
+            {
+                audio.clip = didgerido;
+                audio.Play();
+            }
+        }
     }
 
     //Get a random number from 1-3
