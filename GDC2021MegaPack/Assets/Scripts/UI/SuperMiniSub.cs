cd93219b752b4b2c008e0866a3dd9ece9c716402
GDC2021-GR2-PROG0 <teknisk@game.unf.dk>
2021-07-26 20:45:19 +0200

    UI miniSub er lavet færdig med Mask, der er lavet adjustments på pauseskærmen og vi er begyndt på at lave bobler efter ubåden

--- GDC2021MegaPack/Assets/Scripts/UI/SuperMiniSub.cs
+++ GDC2021MegaPack/Assets/Scripts/UI/SuperMiniSub.cs
@@ -5,17 +5,40 @@ using UnityEngine.UI;
 
 public class SuperMiniSub : MonoBehaviour
 {
+    private RectTransform myRect;
+
+    public float rotateSpeed = 1f;
+
+    public float rotateDegrees = 5f;
+
+    private float turnDirection = 1f;
+
     void Start()
     {
-        // GetComponent<RectTransform>().anchoredPosition = new Vector2(GetComponent<RectTransform>().anchoredPosition.x, 20f);
+        myRect = GetComponent<RectTransform>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        // Får et tal med hvor langt nede UI ubåden skal være på måleren
         float currentProgress = (ScoreHandler.playerScore - 0f) / (ScoreHandler.endDarkValue - 0f);
         float newYCoordinate = Mathf.Lerp(185f, -193f, currentProgress);
 
-        GetComponent<RectTransform>().localPosition = new Vector2(GetComponent<RectTransform>().anchoredPosition.x, newYCoordinate);
+        // Rykker UI ubåden ned
+        myRect.localPosition = new Vector2(myRect.anchoredPosition.x, newYCoordinate);
+
+        // Ændrer retning når den når rotateDegrees vinkel
+        if (myRect.localEulerAngles.z >= rotateDegrees && myRect.localEulerAngles.z < 180)
+        {
+            turnDirection = -1f;
+        }
+        else if (myRect.localEulerAngles.z <= 360 - rotateDegrees && myRect.localEulerAngles.z > 180)
+        {
+            turnDirection = 1f;
+        }
+
+        // Roterer UI ubåden
+        myRect.localEulerAngles = new Vector3(myRect.localEulerAngles.x, myRect.localEulerAngles.y, myRect.localEulerAngles.z + rotateSpeed * Time.deltaTime * turnDirection);
     }
 }
